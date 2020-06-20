9cce92e9f25c468ea5f10fe46254036d28f370ad
Doug Sewell <DouglasASewell@gmail.com>
2020-06-19 23:00:16 -0400

    Initial look of the header completed.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import BackgroundPhoto from './assets/header/header-background.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    header: {
+      headerPrimary: 'Pizza Place',
+      headerSecondary: 'Making Great Pizza',
+      backgroundImage: BackgroundPhoto,
+      gradient:['to bottom','rgba(139,0,0,0.9)','rgba(136, 65, 65, 0.9)']
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header
+          primary={this.state.header.headerPrimary}
+          secondary={this.state.header.headerSecondary}
+          background={this.state.header.backgroundImage}
+          gradient={this.state.header.gradient} />
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Header.module.css';
+//Signage phto for background here: https://www.pexels.com/photo/red-pizza-neon-signage-2914550/
+
+const header = props => {
+
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(${props.gradient[0]},${props.gradient[1]},${props.gradient[2]}),url(${props.background})`
+    }
+
+    console.log(props.gradient)
+
+    return (
+        <header
+            style={backgroundStyle}
+            className={classes.Header}>
+            <div className={classes.TextBox}>
+                <h1 className={classes.HeaderPrimary}>
+                    <span className={classes.HeaderPrimaryMain}>{props.primary}</span>
+                    <span className={classes.HeaderPrimarySub}>{props.secondary}</span>
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default header;
